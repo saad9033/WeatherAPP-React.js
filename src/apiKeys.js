e3acd07e461c96b13492a32e1c03b1752454075d
saad9033 <164220285+saad9033@users.noreply.github.com>
2024-07-10 11:23:07 +0530

    Weather app added

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "f2611060b52df7b6111bd3099bbe7e90",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
